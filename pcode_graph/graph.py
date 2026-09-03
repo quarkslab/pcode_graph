c90b135e5e0ea0fbf9c9dd2879154c6ff57ea8b9
Samuel Hangouët <samuel.hangouet@gmail.com>
2026-09-03 15:41:14 +0200

    Fix regression for older python version

--- pcode_graph/graph.py
+++ pcode_graph/graph.py
@@ -105,7 +105,7 @@ class Node:
 class EdgeProxy:
     """Easy access to edges for one node."""
 
-    graph: CDG
+    graph: "CDG"
     in_edge_indexes: list[EdgeIndex]
     out_edge_indexes: list[EdgeIndex]
 
